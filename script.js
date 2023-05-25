1cfada24c8d8b30d5461815734322aa33e38f847
LAYHAK <layhak.garu@gmail.com>
2023-05-25 09:55:13 +0700

    Youtube Clone project

--- script.js
+++ script.js
@@ -1,6 +1,18 @@
-// const menuIconButton = document.querySelector("[data-menu-icon-btn]")
-// const sidebar = document.querySelector("[data-sidebar]")
+const menuIconButton = document.querySelector("[data-menu-icon-btn]");
+const sidebar = document.querySelector("[data-sidebar]");
 
-// menuIconButton.addEventListener("click", () => {
-//   sidebar.classList.toggle("open")
-// })
+menuIconButton.addEventListener("click", () => {
+  sidebar.classList.toggle("open");
+});
+
+//change body backgroundcolor  using javascript
+
+//create class product
+
+class Product {
+  constructor(name, price, year) {
+    this.name = name;
+    this.price = price;
+    this.year = year;
+  }
+}
